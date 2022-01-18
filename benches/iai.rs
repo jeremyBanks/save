c66373584df9918f351bfdf7d99d0054b410ee7d
Jeremy Banks <save>
2022-01-18 02:33:36 +0000

    r137/c663/5225

--- benches/iai.rs
+++ benches/iai.rs
@@ -1,6 +1,13 @@
-use {
+#![feature(const_eval_limit)]
+#![const_eval_limit = "0"]
+
+pub use {
     crossterm::style::{style, Color, Stylize},
     iai::black_box,
+};
+use {
+    git2::{ObjectType, Oid},
+    save::git2::OidExt,
 };
 
 #[macro_export]
@@ -11,6 +18,11 @@ macro_rules! main {
 
         #[cfg(not(all(unix, not(target_os = "macos"))))]
         fn main() {
+            unsupported_main()
+        }
+
+        #[allow(unused)]
+        fn unsupported_main() {
             let warning = style("warning").with(Color::Yellow);
             let skipping = style("    Skipping").with(Color::Green);
             eprintln!("{warning}: skipping `iai` benches because valgrind is not supported on this platform");
@@ -19,13 +31,47 @@ macro_rules! main {
         }
     }
 }
-
 fn bench_hash_object_git2() {
-    black_box(2);
+    black_box(Oid::hash_object(
+        ObjectType::Commit,
+        black_box(SMALL_BODY.as_ref()),
+    ))
+    .ok();
 }
 
 fn bench_hash_object_save() {
-    black_box(3);
+    black_box(Oid::for_object("commit", black_box(SMALL_BODY.as_ref())));
 }
 
-main!(bench_hash_object_git2, bench_hash_object_save);
+fn bench_hash_object_git2_large() {
+    black_box(Oid::hash_object(
+        ObjectType::Commit,
+        black_box(LARGE_BODY.as_ref()),
+    ))
+    .ok();
+}
+
+fn bench_hash_object_save_large() {
+    black_box(Oid::for_object("commit", black_box(LARGE_BODY.as_ref())));
+}
+
+main!(
+    bench_hash_object_git2,
+    bench_hash_object_save,
+    bench_hash_object_git2_large,
+    bench_hash_object_save_large
+);
+
+const SMALL_BODY: [u8; 512] = assorted_bytes();
+const LARGE_BODY: [u8; 1_048_576] = assorted_bytes();
+
+const fn assorted_bytes<const LENGTH: usize>() -> [u8; LENGTH] {
+    let mut bytes = [0u8; LENGTH];
+    let mut i = 0;
+    while i < LENGTH {
+        bytes[i] = (i as u8) % 241;
+        i += 1;
+    }
+
+    bytes
+}
