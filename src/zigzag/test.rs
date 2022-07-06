8a9e240d35d23bb9ed737af73534884dae49c8bf
Jeremy Banks <_@jeremy.ca>
2022-07-06 01:22:42 +0000

    r181 / n183 / x8A9E

--- src/zigzag/test.rs
+++ src/zigzag/test.rs
@@ -1,13 +1,13 @@
 use super::*;
 
 #[track_caller]
-fn assert_debug_eq(expected: &'static str, actual: impl ::core::fmt::Debug) {
-    ::expect_test::expect(expected).assert_eq(&format!("{actual:?}"));
+fn assert_debug_eq(expected: impl expect_test::ExpectedData, actual: impl core::fmt::Debug) {
+    expect_test::expect(expected).assert_eq(&format!("{actual:?}"));
 }
 
 #[track_caller]
-fn assert_at(path: &'static str, actual: impl ::core::fmt::Debug) {
-    ::expect_test::expect_file(path).assert_eq(&format!("{actual:?}"))
+fn assert_at(path: &'static str, actual: impl ::core::fmt::Display) {
+    ::expect_test::expect_file(path).assert_eq(&format!("{actual}"))
 }
 
 #[test]
